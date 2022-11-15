ce5779c7178adafdbc8a81014a869b61668b43d4
Kinyua <alfkinyua@gmail.com>
2022-11-15 19:23:18 +0300

    update sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    validates :name, presence: true
-    validates :email, presence: true
-  
-    has_many :categories, dependent: :destroy
-    has_many :groups, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true
+
+  has_many :categories, dependent: :destroy
+  has_many :groups, dependent: :destroy
 end
