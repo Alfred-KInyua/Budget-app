3dc5344bc6d5150044bbf8637b4e0e695e8ce153
Kinyua <alfkinyua@gmail.com>
2022-11-15 14:23:10 +0300

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :groups, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true
 end
